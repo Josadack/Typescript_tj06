0b2e85113b96cb08f0f7512f86137813323805c9
unknown <josadaqueferreira@outlook.com>
2024-12-08 02:25:12 -0300

    Aula 01 Calculadora

--- calculadora/Calculadora.ts
+++ calculadora/Calculadora.ts
@@ -5,7 +5,7 @@ let numero2: number;
 
 numero1 = readlinesync.questionFloat("Digite o primeiro numero: ");
 numero2 = readlinesync.questionFloat("Digite o segundo numero: ");
-
+//
 console.log("A soma dos 2 número é igual a ", numero1+numero2)
 console.log("A subtração dos 2 número é igual a ", numero1-numero2)
 console.log("A multiplicação dos 2 número é igual a ", numero1*numero2)
