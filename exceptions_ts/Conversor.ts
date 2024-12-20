353cf5d64135c3641608de4b6cb44e13d59b57c0
unknown <josadaqueferreira@outlook.com>
2024-12-19 22:14:15 -0300

    Exercicio com Exceptions

---
+++ exceptions_ts/Conversor.ts
@@ -0,0 +1,21 @@
+﻿
+
+
+try{
+    const resultado = converte("Abc");
+    console.log(resultado);
+
+}catch(error: any){
+    if(error instanceof TypeError)
+        console.error('você digitou um valor que não do tipo string')
+    else
+       console.error('\nErro Inesperado', error.message);
+   
+}finally{
+    console.log("programa finalizado!")
+}
+export function converte(conteudo: any): string {
+    return conteudo.toUpperCase();
+
+
+}
---
+++ exceptions_ts/TestaExcecaoSimples.ts
@@ -0,0 +1,33 @@
+﻿import readline = require("readline-sync");
+import { ExcecaoSimples } from "./ExcecaoSimples";
+
+let resultado: number | null;
+let loop: boolean = true;
+
+do {
+       try{
+        let numero1: number = readline.questionFloat("Digite o primeiro numero: ");
+        let numero2: number = readline.questionFloat("Digite o segundo numero: ");
+
+        resultado = dividir(numero1, numero2);
+
+        loop = false;
+       }catch(error: any){
+         if(error instanceof ExcecaoSimples){
+            console.error(error.name);
+            console.error(error.message);
+            console.error("\n Tente novamente")
+         }else
+            console.error("\n Error Inesperado")
+       }
+
+} while (loop);
+
+function dividir(numero1: number, numero2: number): number | null {
+
+    if (numero2 == 0)
+        throw new ExcecaoSimples("Não Existe divisão por zero!");
+
+    return numero1 / numero2;
+
+}
